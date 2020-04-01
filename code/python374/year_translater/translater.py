d5756eb93071f7f2e51685b3270f957363971083
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-01 15:18:28 +0900

    [update] yeat_translater
    ・リファクタリングを行いました。
    ・READMEを編集しました。

--- code/python374/year_translater/translater.py
+++ code/python374/year_translater/translater.py
@@ -1,15 +1,18 @@
 class Translater:
 
     def __init__(self):
+
         from datetime import datetime
         now = datetime.now().year
         self.era = {"taisho":1912, "showa":1926, "heisei":1989, "reiwa": 2019, "now":now}
         self.era_name = ["明治以前", "大正", "昭和", "平成", "令和"]
 
-    def year_to_jp_era(self):
-        year = int(input("西暦年を半角数字で入力してください(例：1990)\n"))
 
+    def year_to_jp_era(self):
+
+        year = int(input("西暦年を半角数字で入力してください(例：1990)\n"))
         era = self.era
+
         if year < era["taisho"]:
             print(str(year) + " 年は、明治以前のため変換未対応です")
         elif year < era["showa"]:
@@ -34,8 +37,13 @@ class Translater:
             jp_year = (year + 1) - era["reiwa"]
             print("令和が続いている場合、" + str(year) + "年は、令和 " + str(jp_year) + " 年です")
 
+
     def jp_era_to_year(self):
-        print("元号を数字で選択してください")
+
+        def result(era_name, jp_year, year):
+            return era_name + str(jp_year) + "年は、西暦 " + str(year) + " 年です"
+
+        print("元号を半角数字で選択してください")
         selected_era = int(input("0:明治以前, 1:大正, 2:昭和, 3:平成, 4:令和\n"))
         era_name = self.era_name
 
@@ -44,16 +52,19 @@ class Translater:
             return ""
 
         jp_year = int(input((era_name[selected_era] + "の何年か、数字を入力してください\n")))
-
-        if era_name[selected_era] == "大正":
+        
+        if era_name[selected_era] == era_name[1]:
             year = 1912 + (jp_year - 1)
-            print(era_name[selected_era] + str(jp_year) + "年は、西暦 " + str(year) + " 年です")
-        elif era_name[selected_era] == "昭和":
+            print(result(era_name[selected_era], jp_year, year))
+        elif era_name[selected_era] == era_name[2]:
             year = 1926 + (jp_year - 1)
-            print(era_name[selected_era] + str(jp_year) + "年は、西暦 " + str(year) + " 年です")
-        elif era_name[selected_era] == "平成":
+            print(result(era_name[selected_era], jp_year, year))
+        elif era_name[selected_era] == era_name[3]:
             year = 1989 + (jp_year - 1)
-            print(era_name[selected_era] + str(jp_year) + "年は、西暦 " + str(year) + " 年です")
-        else:
+            print(result(era_name[selected_era], jp_year, year))
+        elif era_name[selected_era] == era_name[4]:
             year = 2019 + (jp_year - 1)
-            print(era_name[selected_era] + str(jp_year) + "年は、西暦 " + str(year) + " 年です")
+            print(result(era_name[selected_era], jp_year, year))
+        else:
+            print("無効な値が入力されました")
+            self.jp_era_to_year()
