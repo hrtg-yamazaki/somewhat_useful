64e141de70623f6bb75abe8c769ab466ed54843a
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-02 15:41:10 +0900

    [update] vocabulary_notebook
    ・登録と検索結果に意味を追加して、dict型オブジェクトの取り扱いを試行しました
    ・一部変数名を変更

--- code/python374/vocabulary_notebook/main.py
+++ code/python374/vocabulary_notebook/main.py
@@ -1,19 +1,19 @@
 
 from notebook import Notebook
 
-border = "--------------------\n"
+border = "--------------------"
 note = Notebook()
 
 while True:
     print(border)
     print("数字を入力し操作したい項目を選択して下さい")
     choice = int(input
-        (border + "1: 文字列を登録する\n2: 登録した文字列をキーワードで検索する\n0: 操作を終了する\n")
+        (border + "\n1: 単語を登録する\n2: 登録した単語をキーワードで検索する\n0: 操作を終了する\n")
     )
     if choice == 1:
         note.regist()
     elif choice == 2:
-        if not note.str_list:
+        if not note.word_dict:
             print(border)
             print("まだ登録がありません")
         else:
--- code/python374/vocabulary_notebook/notebook.py
+++ code/python374/vocabulary_notebook/notebook.py
@@ -1,17 +1,30 @@
+border = "--------------------"
+
 class Notebook:
 
     def __init__(self):
-        self.str_list = []
+
+        self.word_dict = {}
+
 
     def regist(self):
-        string = input("任意の文字列を入力してください\n")
-        self.str_list.append(string)
-        print("新たに [" + string + "] が登録されました")
+
+        word = input("登録したい単語、文字列を入力してください\n")
+        mean = input("入力した単語の意味、説明を入力してください\n")
+        self.word_dict.update({word: mean})
+        print("新たに [ " + word + " ] が登録されました")
+
 
     def search(self):
-        searched = []
+
+        dicts = {}
         keyword = input("キーワードを入力してください\n")
-        for string in self.str_list:
-            if keyword in string:
-                searched.append(string)
-        print(keyword + " を含む文字列は以下の通りです\n" + "\n".join(searched))
+        for key in self.word_dict:
+            if keyword in key:
+                d = {key: self.word_dict[key]}
+                dicts.update(d)
+
+        print(border)
+        print("【検索結果一覧】")
+        for key in dicts:
+            print("・" + key + "\n意味： " + dicts[key])
