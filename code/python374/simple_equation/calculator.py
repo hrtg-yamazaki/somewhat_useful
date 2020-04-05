a86aa4b0f5242934cabc23945e5573540f9a8759
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-05 16:01:40 +0900

    [add] simple_equation
    簡単な一次方程式の解を算出するプログラム

---
+++ code/python374/simple_equation/calculator.py
@@ -0,0 +1,32 @@
+def message():
+
+    s_1 = "簡単な方程式の解を算出します\n方程式を入力してください\n"
+    s_2 = "フォーマットは以下の通りです：\n例１： 5 + 5 = x\n例２： 5 - x = 5\n例３： x + 5 = 5"
+    return s_1 + s_2
+
+
+def answer(a, op, b, eq, c):
+
+    if a == "x":
+        b = int(b)
+        c = int(c)
+        if op == "+":
+            answer = c - b
+        else:
+            answer = c + b
+    elif b == "x":
+        a = int(a)
+        c = int(c)
+        if op == "+":
+            answer = c - a
+        else:
+            answer = c + a
+    else:
+        a = int(a)
+        b = int(b)
+        if op == "+":
+            answer = a + b
+        else:
+            answer = a - b
+
+    return answer
---
+++ code/python374/simple_equation/main.py
@@ -0,0 +1,14 @@
+from calculator import message, answer
+
+print(message())
+
+input_line = input("").split(" ")
+a = input_line[0]
+op = input_line[1]
+b = input_line[2]
+eq = input_line[3]
+c = input_line[4]
+
+x = answer(a, op, b, eq, c)
+
+print("x の値は " + str(x) + " です")
