7f7d5a01f21d79314af6d48cc96a265d590febbe
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-05 16:44:34 +0900

    [update] simple_equation
    掛け算と割り算に対応しました。

--- code/python374/simple_equation/calculator.py
+++ code/python374/simple_equation/calculator.py
@@ -1,7 +1,7 @@
 def message():
 
     s_1 = "簡単な方程式の解を算出します\n方程式を入力してください\n"
-    s_2 = "フォーマットは以下の通りです：\n例１： 5 + 5 = x\n例２： 5 - x = 5\n例３： x + 5 = 5"
+    s_2 = "フォーマットは以下の通りです：\n例１： 5 + 5 = x\n例２： 5 - x = 5\n例３： x × 5 = 5\n例４： x ÷ 5 = 5"
     return s_1 + s_2
 
 
@@ -12,21 +12,33 @@ def answer(a, op, b, eq, c):
         c = int(c)
         if op == "+":
             answer = c - b
-        else:
+        elif op == "-":
             answer = c + b
+        elif op == "×":
+            answer = c / b
+        else:
+            answer = c * b
     elif b == "x":
         a = int(a)
         c = int(c)
         if op == "+":
             answer = c - a
-        else:
+        elif op == "-":
             answer = c + a
+        elif op == "×":
+            answer = c / a
+        else:
+            answer = c * a
     else:
         a = int(a)
         b = int(b)
         if op == "+":
             answer = a + b
-        else:
+        elif op == "-":
             answer = a - b
+        elif op == "×":
+            answer = a * b
+        else:
+            answer = a / b
 
-    return answer
+    return int(answer)
--- code/python374/simple_equation/main.py
+++ code/python374/simple_equation/main.py
@@ -2,7 +2,7 @@ from calculator import message, answer
 
 print(message())
 
-input_line = input("").split(" ")
+input_line = input().split(" ")
 a = input_line[0]
 op = input_line[1]
 b = input_line[2]
