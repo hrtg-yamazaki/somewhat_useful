3d58fa93096c8e34f6d8dcc3ffea4280e724542a
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-05 17:11:53 +0900

    [update'] simple_equation
    入力フォーマットに関するハンドリング

--- code/python374/simple_equation/main.py
+++ code/python374/simple_equation/main.py
@@ -1,14 +1,33 @@
 from calculator import message, answer
 
-print(message())
+border = "--------------------"
 
-input_line = input().split(" ")
-a = input_line[0]
-op = input_line[1]
-b = input_line[2]
-eq = input_line[3]
-c = input_line[4]
+while True:
+    print(border)
+    print(message())
+    print(border)
+    input_line = input().split(" ")
+    
+    if len(input_line) == 5:
+        a = input_line[0]
+        op = input_line[1]
+        b = input_line[2]
+        eq = input_line[3]
+        c = input_line[4]
 
-x = answer(a, op, b, eq, c)
-
-print("x の値は " + str(x) + " です")
+        if op == "+" or "-" or "×" or "÷":
+            if eq == "=":
+                x = answer(a, op, b, eq, c)
+                print("x の値は " + str(x) + " です")
+                choice = input("プログラムを終了しますか？(y/n)\n")
+                if choice == "y":
+                    print("終了します")
+                    break
+                else:
+                    print("もう一度起動します")
+            else:
+                print("無効なフォーマットで入力されました\n最初からやり直してください")
+        else:
+          print("無効な演算子が入力されました\n最初からやり直してください")
+    else:
+      print("無効なフォーマットで入力されました\n最初からやり直してください")
