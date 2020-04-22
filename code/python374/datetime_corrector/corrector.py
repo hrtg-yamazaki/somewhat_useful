54fd619e23d50b548cf60b7efb734fc3fe9f7718
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-04-22 12:58:31 +0900

    [update] datetime_corrector
    ・フォーマットの誤り(一桁の数値が与えられた時の文字数のズレ)を修正する挙動を関数化

--- code/python374/datetime_corrector/corrector.py
+++ code/python374/datetime_corrector/corrector.py
@@ -1,3 +1,18 @@
+
+def fix_length(target, item, time_list):
+    if target == "minute":
+        i = 9
+    elif target == "hour":
+        i = 6
+
+    if item < 10:
+        time_list[i:(i + 2)] = "0" + str(item)
+    else:
+        time_list[i:(i + 2)] = str(item)
+
+    return time_list
+
+
 def fix_minutes(time):
 
     time_list = list(time)
@@ -5,14 +20,10 @@ def fix_minutes(time):
     minute = int(time[9:11])
     hour = hour + 1
     minute = minute - 60
-    if hour < 10:
-        time_list[6:8] = "0" + str(hour)
-    else:
-        time_list[6:8] = str(hour)
-    if minute < 10:
-        time_list[9:11] = "0" + str(minute)
-    else:
-        time_list[9:11] = str(minute)
+
+    time_list = fix_length("hour", hour, time_list)
+    time_list = fix_length("minute", minute, time_list)
+
     time = "".join(time_list)
 
     return time
