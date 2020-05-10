f4578d17229dc0a467ad7c1ef966f1abec5e8a90
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-05-10 22:18:31 +0900

    [update] study_time_log
    ・勉強時間を登録する機能を実装しました。
    ・READMEに軽微な修正

--- code/python374/study_time_log/main.py
+++ code/python374/study_time_log/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from register import registration
 
 
 print("勉強時間を記録するプログラムです")
@@ -6,7 +7,8 @@ print("勉強時間を記録するプログラムです")
 while True:
     choice = input("1: データを登録する\n2: データを確認する\n0: アプリケーションを終了する\n")
     if choice == "1":
-        print("データを登録(開発中)")
+        print("データを登録する")
+        registration()
     elif choice == "2":
         print("データを閲覧する(開発中)")
     elif choice == "0":
---
+++ code/python374/study_time_log/register.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+
+def registration():
+
+    now = datetime.now()
+    str_now = now.strftime("%Y/%m/%d")
+    this_month = now.strftime("%Y_%m")
+    today = now.strftime("%d")
+
+    # ファイルがなければ作成しておく
+    with open("log/monthes_data.log", "a", encoding="UTF-8") as f:
+        pass
+    with open("log/" + this_month + ".log", "a", encoding="UTF-8") as f:
+        pass
+
+    # 当月の登録があるかどうかの確認
+    registed_month = False
+    with open("log/monthes_data.log", "r") as f:
+        for row in f:
+            month = row.rstrip()
+            if this_month == month:
+                registed_month = True
+
+    # 当月のデータがなければ、登録する(このデータは閲覧時に選択肢として利用する)
+    if registed_month == False:
+        with open("log/monthes_data.log", "a", encoding="UTF-8") as f:
+            f.write(this_month + "\n")
+
+    # 当日のデータがあるかどうか確認
+    registed_today = False
+    with open("log/" + this_month + ".log", "r") as f:
+        for row in f:
+            line = row.rstrip().split(" , ")
+            if line[0] == today:
+              registed_today = True
+
+    # 勉強時間の記録(月ごとにログを管理)
+    if registed_today == False:
+        study_time = input("今日は何時間勉強しましたか？半角数字で入力してください\n")
+        with open("log/" + this_month + ".log", "a", encoding="UTF-8") as f:
+            f.write(today + " , " + study_time + "\n")
+        print("登録が完了しました")
+        print("( ", str_now, " の学習時間： ", study_time, " 時間 )", sep="")
+    else:
+        print("本日の登録は終了しています")
