ed1e93401dab58235bb7f073776e70287583a776
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-05-10 20:47:52 +0900

    [add] study_time_log
    ・勉強時間を記録するプログラムを作成するにあたり、まずは最初の選択部分を作成しました。
    ・このディレクトリ用のREADMEの作成を試行します

---
+++ code/python374/study_time_log/main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+print("勉強時間を記録するプログラムです")
+
+while True:
+    choice = input("1: データを登録する\n2: データを確認する\n0:アプリケーションを終了する\n")
+    if choice == "1":
+        print("データを登録(開発中)")
+    elif choice == "2":
+        print("データを閲覧する(開発中)")
+    elif choice == "0":
+        print("アプリケーションを終了します")
+        break
+    else:
+        print("無効な値が入力されました")
+        print("操作をやり直してください")
