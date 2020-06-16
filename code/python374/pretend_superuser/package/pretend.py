b552f92fcf5ca9c4f4df2fab48e041374c7cfd42
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-06-16 17:25:54 +0900

    [update] pretend_superuser
    ・ユーザーインスタンス作成時に入力を受け取れるようにしました。

--- code/python374/pretend_superuser/package/pretend.py
+++ code/python374/pretend_superuser/package/pretend.py
@@ -2,6 +2,22 @@ from .user import User
 
 
 def main():
-    print("Now developing....")
-    user = User("testuser", "test@test.com", "password")
-    user.print_data()
+    """
+    パッケージモジュールのメイン実行関数。
+    現在開発中...
+    """
+    # 入力を受け取る
+    username = input("Username: ")
+    email = input("Email address: ")
+    password = input("Password: ")
+    password_conf = input("Password (again): ")
+    # password1, 2の入力内容一致確認
+    if password == password_conf:
+        # 一致していればUserインスタンス作成
+        user = User(username, email, password)
+        print("Pretend to create superuser successfully.")
+        user.print_data()
+    else:
+        # していなければメッセージを表示して再帰
+        print("Your passwords didn\'t match.")
+        main()
